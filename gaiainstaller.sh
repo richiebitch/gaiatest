ad356f87dc01929576c01564c733384a7789bacb
abhiag <36300569+abhiag@users.noreply.github.com>
2025-02-23 23:46:51 +0530

    Update gaiainstaller.sh

--- gaiainstaller.sh
+++ gaiainstaller.sh
@@ -164,20 +164,12 @@ if [[ "$gaianet_info" == *"Node ID"* || "$gaianet_info" == *"Device ID"* ]]; the
     if sudo lsof -i :8080 > /dev/null 2>&1; then
         echo -e "\e[1;32m✅ GaiaNode is active. GaiaNet node is running.\e[0m"
     else
-        echo -e "\e[1;31m❌ GaiaNode is not running. Please start your GaiaNode first.\e[0m"
-        echo -e "\e[1;33m🔍 Please start your GaiaNet node first.\e[0m"
+        echo -e "\e[1;31m❌ GaiaNode is not running.\e[0m"
+        echo -e "\e[1;33m🔍 Please Back to Main Menu & Restart your GaiaNet node first.\e[0m"
         echo -e "\e[1;33m🔗 Check: \e[1;34mhttps://www.gaianet.ai/setting/nodes\e[0m"
         read -p "Press Enter to return to the main menu..."
         continue
     fi
-else
-    echo -e "\e[1;31m❌ GaiaNet is installed but not functioning correctly. Please verify installation.\e[0m"
-    echo -e "\e[1;33m🔍 Run: \e[1;32m'gaianet info'\e[0m to confirm."
-    echo -e "\e[1;33m🔗 Check: \e[1;34mhttps://www.gaianet.ai/setting/nodes\e[0m"
-    read -p "Press Enter to return to the main menu..."
-    continue
-fi
-
             # Function to check if the system is a VPS, laptop, or desktop
             check_if_vps_or_laptop() {
                 vps_type=$(systemd-detect-virt)
